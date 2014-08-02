651d8eee06b5bf19c3ab0ff3158f2f11a1b30659
Logan Allen <loganallc@gmail.com>
2014-08-02 12:58:28 -0400

    Updates to package.json

--- gulpfile.js
+++ gulpfile.js
@@ -11,28 +11,28 @@ var jsDir = distDir + "/js";
 var cssDir = distDir + "/css";
 
 gulp.task("browserify", function () {
-    gulp.src("app/app.js", {entry: true})
-        .pipe(browserify({
-            transform: [
-                "debowerify", "jadeify"
-            ]
-        }))
-        .pipe(gulp.dest(jsDir));
+  gulp.src("app/app.js", {entry: true})
+  .pipe(browserify({
+    transform: [
+      "jadeify"
+    ]
+  }))
+  .pipe(gulp.dest(jsDir));
 });
 
 gulp.task("html-index", function () {
-    gulp.src(files.html.index)
-        .pipe(jade({
-            dev: false
-        }))
-        .pipe(gulp.dest(distDir));
+  gulp.src(files.html.index)
+  .pipe(jade({
+    dev: false
+  }))
+  .pipe(gulp.dest(distDir));
 });
 
 gulp.task("copy", function () {
-    gulp.src(files.copy.fonts)
-        .pipe(gulp.dest(distDir + "/fonts"));
-    gulp.src(files.copy.img)
-        .pipe(gulp.dest(distDir + "/img"));
+  gulp.src(files.copy.fonts)
+  .pipe(gulp.dest(distDir + "/fonts"));
+  gulp.src(files.copy.img)
+  .pipe(gulp.dest(distDir + "/img"));
 });
 
-gulp.task("build", ["browserify", "html-index", "stylus", "vendor-css", "copy"]);
+gulp.task("build", ["browserify", "html-index", "copy"]);
